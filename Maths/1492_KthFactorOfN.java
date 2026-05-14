15893205b69d45d575400fedb75b27e82473a904
Astha Chhablani <asthachhablani05@gmail.com>
2026-05-14 13:26:13 +0530

    Added kth Factor of n solution

---
+++ Maths/1492_KthFactorOfN.java
@@ -0,0 +1,22 @@
+// Problem: The kth Factor of n
+// Link: https://leetcode.com/problems/the-kth-factor-of-n/
+// Approach: Count factors while traversing
+// TC: O(n)
+
+class Solution {
+    public int kthFactor(int n, int k) {
+        int count = 0;
+
+        for(int i = 1; i <= n; i++){
+            if(n % i == 0){
+                count++;
+
+                if(count == k){
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
