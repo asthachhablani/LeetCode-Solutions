8944659b4b22ea622e5efcf945bf899554a693d7
Astha Chhablani <asthachhablani05@gmail.com>
2026-05-24 05:38:18 +0530

    Added Find Peak Element solution

---
+++ BinarySearch/162_FindPeakElement.java
@@ -0,0 +1,26 @@
+// Problem: Find Peak Element
+// Link: https://leetcode.com/problems/find-peak-element/
+// Approach: Binary Search
+// TC: O(log n)
+
+class Solution {
+    public int findPeakElement(int[] nums) {
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while(start < end){
+
+            int mid = start + (end - start) / 2;
+
+            if(nums[mid] < nums[mid + 1]){
+                start = mid + 1;
+            }
+            else{
+                end = mid;
+            }
+        }
+
+        return end;
+    }
+}
